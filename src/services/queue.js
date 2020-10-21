46d58990292146d3c1fcaf4d86e96797d27cd81f
Bruno Falletti <brunoftti@gmail.com>
2020-10-20 22:53:20 -0300

    Minor change in reinsertion. Corrected comments.

--- src/services/queue.js
+++ src/services/queue.js
@@ -8,17 +8,17 @@ class Queue {
     this.pending = {};
   }
 
-  // If the message exists in pending, removes it and reinserts it as the first of the queue
+  // If the message exists in pending, remove it and reinsert it as the first of the queue
   reinsertMessage (msg) {
     const { messageId } = msg;
 
-    // Searches the message in pending
+    // Search the message in pending
     if (this.pending[`${messageId}`]) {
-      // Reinserts the message as the first of the queue
-      this.messages.unshift(msg);
-
-      // Deletes the message from pending
+      // Delete the message from pending
       delete this.pending[`${messageId}`];
+
+      // Reinsert the message as the first of the queue
+      this.messages.unshift(msg);
 
       if (debug) console.log(`${messageId} reinserted after timeout`);
     }
@@ -46,7 +46,7 @@ class Queue {
   };
 
   // Remove the first selected amount of messages from the queue
-  // Add the messages to a pending object by its ID
+  // Add these messages to pending individually using their IDs as keys
   // For each removed message set a timeout to reinsert it in the queue in the same order
   dequeue (body) {
     const { amount } = body;
@@ -58,7 +58,7 @@ class Queue {
     // Splice is faster than shift when there is a large amount of elements in the queue
     const result = this.messages.splice(0, amount);
 
-    // Add the messages to the pending object
+    // Add the messages to pending
     // Set a reinsertion timeout in reverse order to keep the current message order
     for (let i = result.length - 1; i >= 0; i--) {
       const message = result[i];
